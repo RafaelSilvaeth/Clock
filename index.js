dc26696381b8f75c79e4662231c7d71e70fa0950
Rafael Silva <113713067+RafaelSilvaeth@users.noreply.github.com>
2023-01-05 15:58:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ setInterval(function(){
         let date = new Date();
         let hours =  date.getHours();
         let minutes = date.getMinutes();
-        let seconds =date.getSeconds();
+        let seconds = date.getSeconds();
         let day_night = "AM"
 
         if(hours > 12) {
